14788907938f833117f0b6fb01faea92c6e1bd14
NGIYEHG <georgengiye3@gmail.com>
2024-11-18 13:59:13 +0300

    Another change

--- lambda.py
+++ lambda.py
@@ -1,7 +1,6 @@
-def multiply(x,y)
-:
+def multiply(x,y):
   return x*y
 print(multiply(3,4))
-
+#this function can be implementedd using the lambda function as follows
 multiply=lambda x,y:x**y
 print(multiply(3,4))
