72235af1181971a3e8f905bc8b95cac30a0c354a
NGIYEHG <georgengiye3@gmail.com>
2024-11-18 14:15:14 +0300

    Change

---
+++ Exponentiation.py
@@ -0,0 +1,9 @@
+def exponent(x,y):
+    return x**y        #expontiation operator **
+result=exponent(10,3)  #function call
+print(result)          #printing the result in console,the output is 1000
+
+# Implementing using a lambda function
+# exponent=lambda x,y:x**y
+# result=exponent(10,3)
+# print(result)
